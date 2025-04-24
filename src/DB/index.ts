0da9a45eb65c9eb18c2ea0601b8b666deb4247d3
nimmitahsin1727@gmail.com <nimmitahsin1727@gmail.com>
2025-04-24 19:01:22 -0400

    feat: DB connection with execute query

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -1,18 +1,47 @@
+import { Pool } from 'pg';
+
 class Database {
-	private static instance: string | null = null;
+	private static instance: Pool;
 	private constructor() {}
 
-	public static getInstance() {
+	public static getInstance(): Pool {
 		if (!this.instance) {
 			this.createInstance();
 		}
-		console.log(this.instance);
+
+		return this.instance;
 	}
 
 	private static createInstance() {
-		Database.instance = 'Postgres Database instance created';
-		console.log('Database instance called');
+		Database.instance = new Pool({
+			host: 'localhost',
+			port: 5432,
+			user: 'postgres',
+			password: 'root',
+			database: 'postgres',
+		});
+		console.log('PostgreSQL: Pool created successfully.');
+
+		Database.instance.on('error', (err: Error) => {
+			console.error(err, 'PostgreSQL: Unexpected error on idle client');
+			process.exit(-1);
+		});
 	}
+
+	public static executeQuery = async <T>(query: string): Promise<T[]> => {
+		const pool = Database.getInstance();
+		const client = await pool.connect();
+
+		try {
+			const result = await client.query(query);
+			return result.rows;
+		} catch (err) {
+			console.error(err, 'Error executing query');
+			throw err;
+		} finally {
+			client.release();
+		}
+	};
 }
 
 export default Database;
